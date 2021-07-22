953298a07ac6c7a6dd8010bcd4a1956be688151d
Corinne <corinne.boesch@gmail.com>
2021-07-22 13:24:54 +0100

    updating fizzbuzz.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,21 @@
 def fizzbuzz(n)
-  return 'fizz'
+  case
+  when n % 15 == 0 then return 'fizzbuzz'
+  when n % 3 == 0 then return 'fizz'
+  when n % 5 == 0 then return 'buzz'
+  else return n
+  end
 end
+
+
+
+# def fizzbuzz(number)
+#   (1..number).each { |n|
+#     case
+#     when n % 15 == 0 then puts 'FizzBuzz'
+#     when n % 3 == 0 then puts 'Fizz'
+#     when n % 5 == 0 then puts 'Buzz'
+#     else puts n
+#     end
+#   }
+# end
